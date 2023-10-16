6828a6e38e72e1998c5916a915036e0db17659ef
MiguelBochi <miguelbochibarro@gmail.com>
2023-10-15 22:12:58 -0300

    alteracoes de css

--- src/views/Animal/AnimalDetail.js
+++ src/views/Animal/AnimalDetail.js
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState, useCallback } from "react";
+import { Card, Text, FAB } from "react-native-paper";
+import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
 import animalService from "../../services/animals";
+import ConsultationService from "../../services/consultations";
 const AnimalDetail = ({ route }) => {
   const animalId = route.params.animal;
 
@@ -15,13 +17,97 @@ const AnimalDetail = ({ route }) => {
     fecthAnimal();
   }, [animalId]);
 
+  const [refreshing, setRefreshing] = useState(false);
+  const [consultations, setConsultations] = useState([]);
+
+  const fetchConsultations = async () => {
+    const data = await ConsultationService.getAllConsultation();
+    setConsultations(data);
+  };
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await fetchConsultations();
+    setRefreshing(false);
+  }, []);
+
+  useEffect(() => {
+    fetchConsultations();
+  }, []);
+
   return (
     <View style={styles.container}>
       {animal.id && (
         <>
-          <Text>{animal.id}</Text>
-          <Text>{animal.nome}</Text>
-          <Text>{animal.raca.nome}</Text>
+          <View
+            style={{
+              width: 400,
+              height: 200,
+              backgroundColor: "#667338",
+              borderBottomLeftRadius: 200,
+              borderBottomRightRadius: 200,
+              alignItems: "center",
+              alignContent: "center",
+              marginBottom: 10,
+            }}
+          >
+            <Image
+              style={{ height: "75%", width: "50%" }}
+              source={{ uri: animal.capa.url }}
+            />
+          </View>
+          <View
+            style={{
+              height: "15%",
+              width: "90%",
+              margin: 5,
+              backgroundColor: "#A1B575",
+              borderRadius: 10,
+              padding: 10,
+            }}
+          >
+            <Text>Informacoes do animal</Text>
+            <View style={{ flexDirection: "row" }}>
+              <View style={{ width: "50%" }}>
+                <Text>{animal.nome}</Text>
+                <Text>{animal.peso}</Text>
+                <Text>{animal.sexo}</Text>
+              </View>
+              <View style={{ width: "50%" }}>
+                <Text>{animal.status}</Text>
+                <Text>{animal.tipo_sanguineo}</Text>
+                <Text>{animal.idade}</Text>
+              </View>
+            </View>
+          </View>
+          <View
+            style={{
+              height: "auto",
+              width: "90%",
+              margin: 5,
+              backgroundColor: "#A1B575",
+              borderRadius: 10,
+              padding: 10,
+            }}
+          >
+            <Text>Consultas</Text>
+            {consultations.map((consultation) => (
+              <TouchableOpacity
+                key={consultation.id}
+                onPress={() => navigation.navigate("ConsultationDetail")}
+              >
+                <Card>
+                  <Card.Content>
+                    <View>
+                      <Text>{consultation.descricao}</Text>
+                      <Text>{consultation.data}</Text>
+                      <Text>{consultation.hora}</Text>
+                    </View>
+                  </Card.Content>
+                </Card>
+              </TouchableOpacity>
+            ))}
+          </View>
         </>
       )}
       <StatusBar style="auto" />
@@ -34,7 +120,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
   },
 });
 
--- src/views/Animal/AnimalList.js
+++ src/views/Animal/AnimalList.js
@@ -30,47 +30,46 @@ export default function AnimalsList({ navigation }) {
 
   return (
     <View style={styles.container}>
+      <View style={{ margin: 20, height: "auto", width: "auto" }}>
+        <Text
+          style={{
+            textTransform: "none",
+            fontSize: 35,
+            fontFamily: "Times New Roman",
+          }}
+        >
+          Seus pets
+        </Text>
+      </View>
       <ScrollView
         refreshControl={
           <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
       >
         {animals.map((animal) => (
-          <Card key={animal.id}>
-            <Card.Content style={styles.CardAnimal}>
+          <Card key={animal.id} style={styles.CardAnimal}>
+            <Card.Content>
               <TouchableOpacity
+                style={styles.CardPart}
                 onPress={() =>
                   navigation.navigate("AnimalDetail", { animal: animal.id })
                 }
               >
-                <View>
-                  <Card.Cover
-                    style={{ height: 100, width: 100 }}
-                    source={{ uri: animal.capa.url }}
-                  />
-                  <View style={styles.CardDados}>
-                    <View style={styles.CardDetalhes}>
-                      <Text>Nome: {animal.nome}</Text>
-                      <Text>Idade: {animal.idade}</Text>
-                      <Text>Sexo: {animal.sexo}</Text>
-                      <Text>Peso: {animal.peso} kg</Text>
-                    </View>
-                    <View style={styles.CardSlash} />
-                    <View style={styles.CardDetalhes}>
-                      <Text>Status:{animal.status}</Text>
-                      <Text>?</Text>
-                      <Text>?</Text>
-                      <Text>?</Text>
-                    </View>
+                <Card.Cover
+                  style={{ height: 100, width: 100 }}
+                  source={{ uri: animal.capa.url }}
+                />
+                <View style={styles.CardDados}>
+                  <View style={styles.CardDetalhes}>
+                    <Text style={styles.TextDados}>Nome: {animal.nome}</Text>
+                    <Text style={styles.TextDados}>Idade: {animal.idade}</Text>
+                    <Text style={styles.TextDados}>Peso: {animal.peso} kg</Text>
                   </View>
-                </View>
-                <View style={styles.CardSlash2} />
-                <View style={styles.CardPart2}>
-                  <View style={styles.CardConsulta}>
-                    <Text>Consulta</Text>
-                  </View>
-                  <View style={styles.CardConsulta}>
-                    <Text>Consulta 2</Text>
+                  <View style={styles.CardSlash} />
+                  <View style={styles.CardDetalhes}>
+                    <Text style={styles.TextDados}>Sexo: {animal.sexo}</Text>
+                    <Text style={styles.TextDados}>?</Text>
+                    <Text style={styles.TextDados}>?</Text>
                   </View>
                 </View>
               </TouchableOpacity>
@@ -83,24 +82,24 @@ export default function AnimalsList({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    textAlign: "center",
+    backgroundColor: "#F2F2F2",
+  },
   CardAnimal: {
-    width: "100%",
-    height: "auto",
+    width: "auto",
     backgroundColor: "#667338",
     borderRadius: 15,
     margin: 10,
   },
   CardPart: {
-    width: "75%",
-    height: "100%",
-    flexDirection: "row",
-  },
-  CardImagem: {
-    height: "auto",
     width: "auto",
+    height: "auto",
+    flexDirection: "row",
+    alignItems: "center",
   },
   CardDados: {
-    width: "50%",
+    width: "80%",
     padding: 10,
     flexDirection: "row",
   },
@@ -115,27 +114,9 @@ const styles = StyleSheet.create({
     textAlign: "center",
     backgroundColor: "black",
   },
-  CardSlash2: {
-    width: "100%",
-    height: 1,
-    margin: 2,
-    textAlign: "center",
-    backgroundColor: "black",
-  },
-  CardPart2: {
-    width: "100%",
-    height: "50%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  CardConsulta: {
-    width: "100%",
-    height: "40%",
-    padding: 10,
+  TextDados: {
+    fontFamily: "Times New Roman",
+    fontSize: 15,
     margin: 5,
-    backgroundColor: "#525A35",
-    borderRadius: 15,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
